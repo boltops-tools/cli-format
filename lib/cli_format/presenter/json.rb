034d1b051d11fbd5f626ba8f12a950d7aa964d9d
Tung Nguyen <tongueroo@gmail.com>
2024-04-23 16:58:49 +0000

    improve json format with no header, simple hash

--- lib/cli_format/presenter/json.rb
+++ lib/cli_format/presenter/json.rb
@@ -3,13 +3,22 @@ require "json"
 class CliFormat::Presenter
   class Json < Base
     def text
-      json_data = @rows.map do |row|
-        item = {}
-        @header.each_with_index do |header, i|
-          item[header] = row[i]
+      json_data = if @header
+        @rows.map do |row|
+          item = {}
+          @header.each_with_index do |header, i|
+            item[header] = row[i]
+          end
+          item
         end
-        item
+      elsif @rows.first.size == 2
+        @rows.each_with_object({}) { |(k, v), h|
+          h[k] = v
+        }
+      else # 1 row
+        @rows
       end
+
       JSON.pretty_generate(json_data)
     end
   end
